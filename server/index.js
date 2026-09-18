6991d576dfdad8a51a101392718e1cdcca00c2f7
RogMonSki <szymek108@gmail.com>
2026-09-18 18:08:13 +0100

    Updated gitignore and fixed bug in server

--- server/index.js
+++ server/index.js
@@ -15,4 +15,4 @@ app.get('/api/health', async (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log('Server running on port ${PORT}'))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
